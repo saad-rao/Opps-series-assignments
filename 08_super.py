642f0ab941dec85a507a92b1ad6355c11262c8a2
Saad_Naseem <raosad786@gmail.com>
2025-05-17 01:28:53 +0500

    Add Person and Teacher classes.

---
+++ 08_super.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self,name):
+        self.name= name
+
+
+class Teacher(Person):
+    def __init__(self,name,subject):
+        super().__init__(name)
+        self.subject= subject
+
+t= Teacher("Saad","Computer science")
+print(t.name)
+print(t.subject)
